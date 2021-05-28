7d94731645d68933bf24e222aa10b72a57742e28
Nayeem M. Muzahid <nayeem.muzahid@northsouth.edu>
2021-05-29 02:19:52 +0600

    created update and delete user api

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,7 +1,62 @@
 const router = require("express").Router();
+const bcrypt = require("bcrypt");
+const User = require("../model/userSchema");
 
 router.get("/", (req, res) => {
   res.send(`Hey it's user route`);
 });
 
+//update user
+
+router.put("/:id", async (req, res) => {
+  if (req.body.userId === req.params.id || req.body.isAdmin) {
+    if (req.body.password) {
+      try {
+        const salt = await bcrypt.genSalt(10);
+        req.body.password = await bcrypt.hash(req.body.password, salt);
+      } catch (error) {
+        res.status(500).json({
+          error: "There was an error updating you password",
+        });
+      }
+    }
+
+    try {
+      const user = await User.findByIdAndUpdate(req.params.id, {
+        $set: req.body,
+      });
+      res.status(200).json({
+        message: "Account has been updated",
+      });
+    } catch (error) {
+      res.status(400).json({
+        error: "There was an error while updating your profile",
+      });
+    }
+  } else {
+    res.status(403).json({
+      error: "You only can update your profile",
+    });
+  }
+});
+
+//delete user
+router.delete("/:id", async (req, res) => {
+  if (req.body.userId === req.params.id || req.body.isAdmin) {
+    try {
+      const user = await User.findByIdAndDelete(req.params.id);
+      res.status(200).json({
+        message: "Account has been deleted",
+      });
+    } catch (error) {
+      res.status(400).json({
+        error: "There was an error while deleting your account",
+      });
+    }
+  } else {
+    res.status(403).json({
+      error: "You only can delete your profile",
+    });
+  }
+});
 module.exports = router;
